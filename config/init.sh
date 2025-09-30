8536c66293e40c1041308f88e448442df3778918
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2025-09-30 16:32:46 +0200

    Provide db name and username in postgres docker logs

--- config/init.sh
+++ config/init.sh
@@ -11,17 +11,17 @@ if [[ $EXISTS == "1" ]]; then
 else
   echo "******CONFIGURING DOCKER DATABASE******"
  
-  echo "Creating user"
+  echo "Creating user $DB_USER"
 psql -U postgres <<-EOSQL
   CREATE ROLE $DB_USER WITH LOGIN ENCRYPTED PASSWORD '${DB_PASS}' SUPERUSER;
 EOSQL
  
-  echo "Creating table"
+  echo "Creating table $DB_NAME"
 psql -U postgres <<-EOSQL
   CREATE DATABASE $DB_NAME WITH OWNER $DB_USER ENCODING 'UNICODE' LC_COLLATE 'C' LC_CTYPE 'C' TEMPLATE template0;
 EOSQL
  
-  echo "Granting privileges to user"
+  echo "Granting privileges to user $DB_USER"
 psql -U postgres <<-EOSQL
   GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
 EOSQL
